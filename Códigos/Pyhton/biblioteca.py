bb03c40f33148287e92bcfa4a312f4eb169b9cb4
Bigodeiro <enzobigber@gmail.com>
2022-06-21 20:43:21 -0300

    Estava aprendendo usar decorators

--- Códigos/Pyhton/biblioteca.py
+++ Códigos/Pyhton/biblioteca.py
@@ -7,15 +7,26 @@ def dec_SLS(func):
 
     return wrapper
 
+def dec_tempo(func):
+    def wrapper(a):
+
+        print("inicio")
+        val = func(a)
+        print("fim")
+        return val
+
+    return wrapper
+
 @dec_SLS
 def testef(b):
     i = 0
-    while i < len(b):
-        b[i] = chr(ord(b[i]) + 1)
-#chr: retorna o caractere correspondente ao numero do input
-#ord: retorna o numero correspondente ao caractere do input
+    while i < len(b):                   #*chr: retorna o caractere correspondente ao numero do input
+        b[i] = chr(ord(b[i]) + 1)       #*ord: retorna o numero correspondente ao caractere do input
         i += 1
     return b
 
+@dec_tempo
+def testea(a):
+    print(a + 1)
 
-print(testef('teste'))
+testea(13)   
