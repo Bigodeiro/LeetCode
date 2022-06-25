c6def6f8c59fe51532b936d36c28358da8bbd738
Bigodeiro <enzobigber@gmail.com>
2022-06-25 02:27:43 -0300

    Operador ternário & For... Else

--- Códigos/Pyhton/biblioteca.py
+++ Códigos/Pyhton/biblioteca.py
@@ -1,4 +1,8 @@
-def dec_SLS(func):
+# Nesse arquivo vou reunir conhecimentos que aprendi ao longo do tempo sobre python que considero facil de esquecer.
+
+
+#!DECORATORS
+def decorator_StringListString(func):
     def wrapper(input):
 
         input = list(input)
@@ -7,17 +11,7 @@ def dec_SLS(func):
 
     return wrapper
 
-def dec_tempo(func):
-    def wrapper(a):
-
-        print("inicio")
-        val = func(a)
-        print("fim")
-        return val
-
-    return wrapper
-
-@dec_SLS
+@decorator_StringListString
 def testef(b):
     i = 0
     while i < len(b):                   #*chr: retorna o caractere correspondente ao numero do input
@@ -25,8 +19,26 @@ def testef(b):
         i += 1
     return b
 
-@dec_tempo
-def testea(a):
-    print(a + 1)
+#!Incialização do código
+"""
+Para permitr que o código seja usado como uma biblioteca mesmo, (sem ser executado quando importado) é necessário fazer essa inicialização, que checará em
+qual contexto o código está sendo lido (no caso, "__main__" indica que ele está sendo lido para ser executado), para que possamos importar apenas as 
+funções se quisermos.
+"""
+if __name__ == "__main__":
 
-testea(13)   
+    print(f"{testef('teste')}\n") #*nota: é possivel usar as diferentes as aspas para conseguir usar o printf propriamente
+
+    #!FOR ... ELSE 
+    #? quando acaba o array o "else" é executado
+    arr = [1,2,3]
+    for x in arr:
+        print("as vezes")
+    else: print(f"sempre \n")
+
+    #! OPERADOR TERNÁRIO
+    score = 10 
+
+        #?opção1 if condição else opção 2
+    result = "passou" if score > 6 else "reprouvou"
+    print(f"{result}\n")
