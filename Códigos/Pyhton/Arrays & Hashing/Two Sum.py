d93cd17ee45b1ecae7d3695cfbf35dc43637eff1
Enzo Bigodeiro <90060995+Bigodeiro@users.noreply.github.com>
2022-07-07 07:25:20 -0300

    Two Sum - CONCLUIDO

--- Códigos/Pyhton/Arrays & Hashing/Two Sum.py
+++ Códigos/Pyhton/Arrays & Hashing/Two Sum.py
@@ -1,1 +1,11 @@
 #https://leetcode.com/problems/two-sum/
+#?Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+#! Versão extramente devagar, porém eficiente em memória
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        for i in range(len(nums)):
+            for j in range(i, len(nums)):
+                if nums[i] + nums[j] == target:
+                    if i != j:
+                        return [i, j]
